4fbda547b8b1edd1af9f97324dcc18aae2d94af5
shuhei1018 <nan10kanaruyxxn@gmail.com>
2021-01-16 10:22:40 +0900

    model generate

---
+++ db/migrate/20210116011919_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      
+      t.string :name, null: false
+      t.integer :status_id, null: false
+      t.text :explanation, null: false
+      t.integer :price 
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
