5a7d330d5d865145048a4a8f330358aa2b4f88cb
shuhei1018 <nan10kanaruyxxn@gmail.com>
2021-01-21 22:27:32 +0900

    guest login (easy)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,14 @@ class ProductsController < ApplicationController
   def index
   end
 
+  def new_guest
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
+
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :products, only: :index
   root to: "products#index"
+  post '/products/guest_sign_in', to: 'products#new_guest'
 end
