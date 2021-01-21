9c7a1d6b09f3dafc74fcb1e8bef03815f7dba0f1
shuhei1018 <nan10kanaruyxxn@gmail.com>
2021-01-21 22:46:47 +0900

    guest login modify

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :products, only: :index
   root to: "products#index"
-  post '/products/guest_sign_in', to: 'products#new_guest'
+  devise_scope :user do
+    post '/users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
 end
