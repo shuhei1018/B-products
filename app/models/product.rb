6acfd480fc06fb237762ca01b49fbbb91c3794c2
shuhei1018 <nan10kanaruyxxn@gmail.com>
2021-01-25 22:53:15 +0900

    image asociation

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,6 @@ class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :status
   belongs_to :user
+  has_one_attached :image
   validates :status_id, numericality: { other_than: 1 }
 end
