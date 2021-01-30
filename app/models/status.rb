27d903cd0ede8f94cc0acf2c0a8bbc758d4ed9f3
shuhei1018 <nan10kanaruyxxn@gmail.com>
2021-01-30 15:43:53 +0900

    product index front rev4

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,8 +1,8 @@
 class Status < ActiveHash::Base
   self.data = [
     { id: 0, name: '0:--' },
-    { id: 1, name: '1:正規品レベル(少量ゆえに販売できていない製品)' },
-    { id: 2, name: '2:正規品に近いレベル(品位が若干規格外の製品)' },
+    { id: 1, name: '1:正規品レベル　　　　　(少量ゆえに販売できていない製品)' },
+    { id: 2, name: '2:正規品に近いレベル　　(品位が若干規格外の製品)' },
     { id: 3, name: '3:傷や汚れが分かるレベル(品位が規格外の製品)' }
   ]
   include ActiveHash::Associations
