9c3e60b39e28bc4daa0dd6d511ec27ec3213881a
shuhei1018 <nan10kanaruyxxn@gmail.com>
2021-02-03 15:09:16 +0900

    update function setting

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -34,6 +34,15 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to product_path(@product.id)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def product_params
