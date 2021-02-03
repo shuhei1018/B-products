c3ba3e9b900aefeee5efcd7049c71a4795770f39
shuhei1018 <nan10kanaruyxxn@gmail.com>
2021-02-03 18:04:41 +0900

    association setting

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :products
+  has_many :comments 
   def self.guest
     find_or_create_by!(nickname: 'GuestUser', email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
