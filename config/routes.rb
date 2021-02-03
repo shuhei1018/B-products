d21289965005dad3bbce9961f78d990cae521448
shuhei1018 <nan10kanaruyxxn@gmail.com>
2021-02-03 18:12:00 +0900

    routing setting

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
-  resources :products
   root to: "products#index"
+  resources :products do
+    resources :comments
+  end
   devise_scope :user do
     post '/users/guest_sign_in', to: 'users/sessions#new_guest'
   end
