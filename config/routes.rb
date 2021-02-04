964a6030d7e2fd9155ebca053f16054fe4687db7
shuhei1018 <nan10kanaruyxxn@gmail.com>
2021-02-04 19:56:43 +0900

    comment routing modified

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   }
   root to: "products#index"
   resources :products do
-    resources :comments
+    resources :comments, only: [:create, :destroy]
   end
   devise_scope :user do
     post '/users/guest_sign_in', to: 'users/sessions#new_guest'
