6c83321516fe87c736b55911935a8e6ef6c57e89
shuhei1018 <nan10kanaruyxxn@gmail.com>
2021-02-12 15:00:23 +0900

    Capistrano setting

---
+++ config/deploy.rb
@@ -0,0 +1,26 @@
+
+lock '3.14.1'
+
+set :application, 'B-products'
+
+set :repo_url,  'git@github.com:shuhei1018/B-products.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.5'
+
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/a11tp027.pem'] 
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
